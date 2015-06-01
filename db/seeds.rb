68fd870f955fa106b42918ca27bf77bc95ff6162
Rhys Williams <rhyswilliams314@gmail.com>
2015-06-02 01:48:03 +1000

    Add calculation of each location's postcode

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,18 @@ require 'open-uri'
 require 'csv'
 
 # Put all postcodes and their associated lat/long into database.
-CSV.foreach("data/postcodes.csv") do |row|
-	hash = Hash.new
-	hash[:postcode] = row[0]
-	hash[:name] = row[1]
-	hash[:latitude] = row[3]
-	hash[:longitude] = row[4]
-	new = Postcode.create(hash)
-  new.save
+csv_file = File.read("data/postcodes.csv")
+CSV.foreach("data/postcodes.csv", converters: :numeric, :headers => true) do |row|
+  Postcode.create!(row.to_hash)
 end
 
 # Retrieve weather staions and associate with postcode.
-@line = Array.new
+@lines = Array.new
 @fields = Array.new
 
 postcodes = Postcode.all
 
-doc = open('ftp://ftp.bom.gov.au/anon2/home/ncc/metadata/lists_by_element/numeric/numVIC_139.txt'){|f| f.read }
+doc = open("ftp://ftp.bom.gov.au/anon2/home/ncc/metadata/lists_by_element/numeric/numVIC_139.txt"){|f| f.read }
 
 text_file = File.new("data_text.txt","w+")
 text_file.write(doc)
@@ -40,12 +35,12 @@ File.open("data_text.txt") do |in_file|
 end # closes test.txt
 
 CSV.foreach("test.csv") do |row|
-  @line << row[0]
+  @lines << row[0]
 end
 
-@line = @line.slice(4..2296)
+@lines = @lines.slice(4..2296)
 
-@fields.each do |line|
+@lines.each do |line|
   @fields << line.split(' ')
 end
 
@@ -58,26 +53,24 @@ end
   end
   
   hash = Hash.new
-  hash[:location_id] = temp[temp.size - 1].to_i
   hash[:longitude] = temp[0].to_f
   hash[:latitude] = temp[1].to_f
-  hash[:name] = name
+  hash[:location_id] = name
 
   closest_dist = Float::MAX
   best_postcode = 0
 
-  postcodes.each do |postcode|
-    new_dist = Math.sqrt((hash[:longitude]-postcode.longitude)**2 + (hash[:latitude]-postcode.latitude)**2)
-    if (new_dist < closest_dist)
-      closest_dist = new_dist
-      best_postcode = postcode.postcode
-    end
-  end
+   postcodes.each do |postcode|
+     new_dist = Math.sqrt((hash[:longitude]-postcode.longitude)**2 + (hash[:latitude]-postcode.latitude)**2)
+     if (new_dist < closest_dist)
+       closest_dist = new_dist
+       best_postcode = postcode.postcode
+     end
+   end
 
   hash[:postcode] = best_postcode
   
-  l = Location.create(hash)
-  l.save
+  Location.create(hash)
 end
 
 File.delete("data_text.txt")
